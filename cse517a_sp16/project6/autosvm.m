0028085f48b7a551820831ded533f9f4b8d16550
mengyanli@ACCOUNTS.AD.WUSTL.EDU <mengyanli@ACCOUNTS.AD.WUSTL.EDU@69bb0705-1875-4a40-87b1-c37a599dc3e3>
2016-04-18 01:19:53 +0000

    omgthis hast o work i mean it

--- cse517a_sp16/project6/autosvm.m
+++ cse517a_sp16/project6/autosvm.m
@@ -13,7 +13,7 @@ function svmclassify=autosvm(xTr,yTr)
 disp('Performing cross validation ...');
 
 
-[bestC,bestP,bestval,allerrs]=crossvalidate(xTr,yTr,'rbf',2.^[-1:0.5:8],2.^[1:0.25:2]);
+[bestC,bestP,bestval,allerrs]=crossvalidate(xTr,yTr,'rbf',2.^[-1:8],2.^[1:2]);
 %[bestC,bestP,bestval,allerrs]=crossvalidate(xTr,yTr,'rbf',2.^[bestC-3:0.5:bestC],2.^[bestP-1:0.25:bestP+1]);
 % 
  
