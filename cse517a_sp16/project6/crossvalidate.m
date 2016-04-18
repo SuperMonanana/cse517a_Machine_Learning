3edf1100400008480445ec34c495f5452c954089
mengyanli@ACCOUNTS.AD.WUSTL.EDU <mengyanli@ACCOUNTS.AD.WUSTL.EDU@69bb0705-1875-4a40-87b1-c37a599dc3e3>
2016-04-18 03:31:21 +0000

    ok

--- cse517a_sp16/project6/crossvalidate.m
+++ cse517a_sp16/project6/crossvalidate.m
@@ -32,12 +32,12 @@ function [bestC,bestP,bestval,allvalerrs]=crossvalidate(xTr,yTr,ktype,Cs,paras)
 Cs_length = length(Cs);
 paras_length = length(paras);
 allvalerrs=zeros(Cs_length,paras_length);
-k=10;
+k=7;
 
 
 %% Split off validation data set
 % YOUR CODE
-indices=crossvalind('Kfold',length(xTr),k);
+indices=crossvalind('Kfold',length(yTr),k);
 %     X=mat2cell(xTr,d,n/k*ones(1,k));
 %     Y=mat2cell(yTr,1,n/k*ones(1,k));
 %% Evaluate all parameter settings
@@ -45,12 +45,13 @@ indices=crossvalind('Kfold',length(xTr),k);
 
 % xVal=X{k};
 % yVal=Y{k};
-  xVal=xTr(:,indices==k);
-  yVal=yTr(:,indices==k);
+  %xVal=xTr(:,indices==k);
+  %yVal=yTr(:,indices==k);
 for j=1:k-1 
-      
-      xTrain=xTr(:,indices==j);
-      yTrain=yTr(:,indices==j);
+      xVal=xTr(:,indices==j);
+      yVal=yTr(:,indices==j);
+      xTrain=xTr(:,indices~=j);
+      yTrain=yTr(:,indices~=j);
 %     xTrain=X{j};
 %     yTrain=Y{j};
     %      start=1+floor((j-1)*n/k);
@@ -63,6 +64,7 @@ for j=1:k-1
     
     for ii=1:Cs_length
         for jj=1:paras_length
+            %[train,test] = crossvalind('LeaveMOut',length(yTr),1);
             svmclassify=trainsvm(xTrain,yTrain,Cs(ii), ktype, paras(jj));
             err=sum(sign(svmclassify(xVal))~=yVal(:))./length(yVal);
             allvalerrs(ii,jj)=allvalerrs(ii,jj)+err;
@@ -74,9 +76,9 @@ end
 % YOUR CODE
     bestval =min(allvalerrs(:));
     [BestCii,BestPjj] = find(allvalerrs==bestval);
-      a=length(BestCii);
-      bestC=Cs(BestCii(a));
-      bestP=paras(BestPjj(a));
+      %a=length(BestCii);
+      bestC=Cs(BestCii(1));
+      bestP=paras(BestPjj(1));
      %bestC=Cs(BestCii(unidrnd(length(BestCii))));
      %bestP=paras(BestPjj(unidrnd(length(BestPjj))));
 
